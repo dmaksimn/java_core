f4e9431bf06bb416a7181f9ec6b53f27c79472c7
raman.lis <Raman.Lis@nuance.com>
2021-03-16 11:49:25 +0300

    bedareva

--- src/main/java/com/raman/lis/task01/bedareva/Task_8.java
+++ src/main/java/com/raman/lis/task01/bedareva/Task_8.java
@@ -3,7 +3,7 @@ package com.raman.lis.task01.bedareva;
 public class Task_8 {
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 3, 2, 1, 2, 6, 5, 4, 3, 3, 2, 1, 1, 3, 3, 3, 4, 2};
+        int[] a = {};
         int[] b = new int[a.length]; // массив с суммами стипендий
         int[] c = new int[a.length]; // промежут. массив с суммами стипендий при возрастании рейтингов
         int[] d = new int[a.length]; // промежут. массив с суммами стипендий при убывании рейтингов
@@ -15,66 +15,73 @@ public class Task_8 {
                 min = a[j];
             }
         }
-        // присваиваем промежуточным массивам и итоговому массиву 1 р, в позициях где ретинг минимальный илм минимальный по отношению к соседям
-        for (int i = 0; i < a.length; i++) {
-            if (i < a.length - 1 && i > 0) {
-                if (a[i] == min || (a[i] <= a[i + 1] && a[i] <= a[i - 1])) {
-                    b[i] = c[i] = d[i] = 1;
-                }
-            }
-            if (i == 0) {
-                if (a[i] == min || a[i] < a[i + 1]) {
-                    b[i] = c[i] = d[i] = 1;
-                }
-                if (a[i] > a[i + 1]) {
-                    b[i] = c[i] = d[i] = 0;
-                }
-            }
-            if (i == a.length - 1) {
-                if (a[i] < a[i - 1]) {
-                    b[i] = c[i] = d[i] = 1;
-                } else {
-                    b[i] = c[i] = d[i] = 0;
-                }
-            }
+        if (a.length == 1) {
+            sum = 1;
+            System.out.println(sum);
         }
-        // заполняем промежут. массив с суммами стипендий при возрастании рейтингов
-        for (int t = 0; t < b.length; t++) {
-            if (c[t] == 1) {
-                for (int l = 0; l < b.length && t + l <= b.length - 2; l++) {
-                    if (a[t + l] < a[t + l + 1]) {
-                        c[t + l + 1] = c[t + l] + 1;
+        if (a.length > 1) {
+            // присваиваем промежуточным массивам и итоговому массиву 1 р, в позициях где ретинг минимальный илм минимальный по отношению к соседям
+            for (int i = 0; i < a.length; i++) {
+                if (i < a.length - 1 && i > 0) {
+                    if (a[i] == min || (a[i] <= a[i + 1] && a[i] <= a[i - 1])) {
+                        b[i] = c[i] = d[i] = 1;
+                    }
+                }
+                if (i == 0) {
+                    if (a[i] == min || a[i] < a[i + 1]) {
+                        b[i] = c[i] = d[i] = 1;
+                    }
+                    if (a[i] > a[i + 1]) {
+                        b[i] = c[i] = d[i] = 0;
+                    }
+                }
+                if (i == a.length - 1) {
+                    if (a[i] < a[i - 1] || a[i] == min) {
+                        b[i] = c[i] = d[i] = 1;
                     } else {
-                        break;
+                        b[i] = c[i] = d[i] = 0;
                     }
                 }
             }
-        }
-        // заполняем промежут. массив с суммами стипендий при убывании рейтингов
-        for (int h = d.length - 1; h >= 0; h--) {
-            if (d[h] == 1) {
-                if (h > 0) {
-                    for (int n = 0; n < d.length && h - n <= b.length - 1; n++) {
-                        if (a[h - n] < a[h - n - 1]) {
-                            d[h - n - 1] = d[h - n] + 1;
+            // заполняем промежут. массив с суммами стипендий при возрастании рейтингов
+            for (int t = 0; t < b.length; t++) {
+                if (c[t] == 1) {
+                    for (int l = 0; l < b.length && t + l <= b.length - 2; l++) {
+                        if (a[t + l] < a[t + l + 1]) {
+                            c[t + l + 1] = c[t + l] + 1;
                         } else {
                             break;
                         }
                     }
                 }
             }
-            if (h == 0) {
-                d[h] = 1;
+            // заполняем промежут. массив с суммами стипендий при убывании рейтингов
+            for (int h = d.length - 1; h >= 0; h--) {
+                if (d[h] == 1) {
+                    if (h > 0) {
+                        for (int n = 0; n < d.length && h - n <= b.length - 1 && h - n >= 1; n++) {
+                            if (a[h - n] < a[h - n - 1]) {
+                                d[h - n - 1] = d[h - n] + 1;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (h == 0) {
+                        d[h] = 1;
+                    }
+                }
             }
+            // сращиваем промежут массивы c и d в единый b. При этом учитывыем, что могут быть разные значения в промеж. массивах на одном и том же месте.
+            for (int y = 0; y < a.length; y++) {
+                b[y] = Math.max(c[y], d[y]);
+            }
+            // высчитываем сумму значений массива b
+            for (int s = 0; s < b.length; s++) {
+                sum = sum + b[s];
+            }
+            System.out.print(sum);
         }
-        // сращиваем промежут массивы c и d в единый b. При этом учитывыем, что могут быть разные значения в промеж. массивах на одном и том же месте.
-        for (int y = 0; y < a.length; y++) {
-            b[y] = Math.max(c[y], d[y]);
-        }
-        // высчитываем сумму значений массива b
-        for (int s = 0; s < b.length; s++) {
-            sum = sum + b[s];
-        }
-        System.out.print(sum);
     }
 }
