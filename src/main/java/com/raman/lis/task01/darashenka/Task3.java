bdca942749ce2812adb28e3c42d18f552790da10
dmaksimn <dmaksimn@gmail.com>
2021-03-23 16:31:47 +0300

    1

---
+++ src/main/java/com/raman/lis/task01/darashenka/Task3.java
@@ -0,0 +1,14 @@
+public class Task3 {
+    public static void main(String... args) {
+        int array[] = {2, 5, 7, 4, 6, 8, 9, 1};
+        int ReverseArray[] = new int[array.length];
+        for (int i = array.length - 1; i > 0; ) {
+            for (int j = 0; j < array.length; j++) {
+                ReverseArray[j] = array[i];
+                i--;
+                System.out.print(ReverseArray[j] + " ");
+            }
+        }
+    }
+}
+
---
+++ src/main/java/com/raman/lis/task01/darashenka/Task6.java
@@ -0,0 +1,30 @@
+public class Task6 {
+    public static void main(String... args) {
+        int matrix[][] = {{5, 4, 3, 6}, {8, 3, 1, 0}};
+        int matrix2[][] = {{5, 4, 3, 6}, {8, 3, 1, 0}};
+        int sum = 0;
+        int sum1 = 0;
+        int diff = 0;
+        int diff1 = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                sum += matrix[i][j];
+                if (i > j)
+                    diff = i - j;
+                else
+                    diff = j - i;
+            }
+            for (int l = 0; l < matrix2.length; l++) {
+                for (int m = 0; m < matrix2.length; m++) {
+                    sum1 += matrix2[l][m];
+                    if (l > m)
+                        diff1 = l - m;
+                    else
+                        diff1 = m - l;
+                }
+            }
+        }
+        System.out.println("matrix sum = " + (sum + sum1));
+        System.out.println("matrix differences = " + (diff + diff1));
+    }
+}
