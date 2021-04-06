da65fa6e3a4c7f8f83af07ca645fe7e547bc1a30
raman.lis <Raman.Lis@nuance.com>
2021-04-06 12:59:37 +0300

    bogdanovich

---
+++ src/main/java/com/raman/lis/task03/bogdanovich/Stock.java
@@ -0,0 +1,11 @@
+package com.raman.lis.task03.bogdanovich;
+
+import java.util.ArrayList;
+
+public class Stock {
+    static ArrayList <Car> cars = new ArrayList<>();
+    static void printCar () {
+
+    }
+
+}
